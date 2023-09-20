ddca9943089bcf0dbe295d52719ab136f227ea76
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-09-20 08:45:34 +0200

    Add files via upload

---
+++ uvodnaVjezba0.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uvodna_vježba___varijable__ispis_podataka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+            a = 100;
+            b = 3;
+            x = a / b;
+            y = a % b;
+
+            Console.WriteLine(a + " dijeljeno sa " + b + " je " + x + " , a ostatak je " + y);
+            Console.ReadKey();
+        }
+    }
+}
